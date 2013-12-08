8a813042e47fafa16919603fedae06d0e16427d3
esedmehmet <esedmehmet@gmail.com>
2013-12-09 01:28:51 +0200

    .

--- src/main/java/webserverim/Response/Cevap.java
+++ src/main/java/webserverim/Response/Cevap.java
@@ -1,10 +1,15 @@
 package webserverim.Response;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
+import java.net.URI;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,10 +21,21 @@ import java.io.OutputStream;
 public class Cevap implements HttpHandler {
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-        String response = "Sayfaya ulasildi";
-        httpExchange.sendResponseHeaders(200,response.length());
-        OutputStream sonuc = httpExchange.getResponseBody();
-        sonuc.write(response.getBytes());
-        sonuc.close();
+        URI uri = httpExchange.getRequestURI();
+        if (uri.getHost() == null) {
+            String response = "Sayfaya ulasildi";
+            httpExchange.sendResponseHeaders(200,response.length());
+            OutputStream sonuc = httpExchange.getResponseBody();
+            sonuc.write(response.getBytes());
+            sonuc.close();
+            sonuc.close();
+        } else {
+            String response = "Sayfaya ulasilamadi";
+            httpExchange.sendResponseHeaders(404,response.length());
+            OutputStream sonuc = httpExchange.getResponseBody();
+            sonuc.write(response.getBytes());
+            sonuc.close();
+            sonuc.close();
+        }
     }
 }
--- src/main/java/webserverim/Webserver.java
+++ src/main/java/webserverim/Webserver.java
@@ -2,7 +2,11 @@ package webserverim;
 
 import com.sun.net.httpserver.HttpServer;
 import webserverim.Response.Cevap;
+
+import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.concurrent.Executors;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,11 +15,12 @@ import java.net.InetSocketAddress;
  * Time: 19:56
  * To change this template use File | Settings | File Templates.
  */
-public class Webserver {
-    public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/deneme", new Cevap());
-        server.setExecutor(null);
+public class Webserver  {
+    public static void main(String[] args) throws IOException {
+        InetSocketAddress port = new InetSocketAddress(8007);
+        HttpServer server = HttpServer.create(port, 0);
+        server.createContext("/Default", new Cevap());
+        server.setExecutor(Executors.newCachedThreadPool());
         server.start();
     }
 }
